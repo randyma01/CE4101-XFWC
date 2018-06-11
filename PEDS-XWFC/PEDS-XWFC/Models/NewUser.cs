57bc5509e01ac46ef2bb04f2eb24c737937a26dc
Gustavo Alonso Fallas Carrera <gfallasc@estudiantec.cr>
2018-06-10 23:28:37 -0600

    Add Model NewUser, language global [ES]

---
+++ PEDS-XWFC/PEDS-XWFC/Models/NewUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PEDS_XWFC.Models
+{
+    public class NewUser
+    {
+        public User GetUser { get; set; }
+        public UserFanatic GetUserFanatic { get; set; }
+    }
+}
