151e5b11ebb8de61ad5a73ec852ce5def0b8169f
Gustavo Alonso Fallas Carrera <gfallasc@estudiantec.cr>
2018-06-11 01:00:57 -0600

    Add theme '_Layout' bootstrap

--- PEDS-XWFC/PEDS-XWFC/Models/UserFanatic.cs
+++ PEDS-XWFC/PEDS-XWFC/Models/UserFanatic.cs
@@ -5,9 +5,16 @@ using System.Web;
 
 namespace PEDS_XWFC.Models
 {
+    
     public class UserFanatic
     {
+        public int IdUser{ get; set; }
+        public int IdCountry { get; set; }
         public int Score{ get; set; }
         public String TelephoneNumber { get; set; }
+        public String Photo { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
     }
 }
