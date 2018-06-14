9ebb2dce8e09160e0beec425a1026724670e7fcb
Randy <randyma01@gmail.com>
2018-06-14 16:35:42 -0600

    Nuevos modelos, vistas y controladores. Cambio en db.

--- PEDS-XWFC/PEDS-XWFC/Models/Sponsor.cs
+++ PEDS-XWFC/PEDS-XWFC/Models/Sponsor.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace PEDS_XWFC.Models
 {
-    
-    public class UserAdmin
+    public class Sponsor
     {
-        public int IdUser { get; set; }
 
+        public int IdSponsor { get; set; }
+        public string NameSponsor { get; set; }
     }
 }
+
