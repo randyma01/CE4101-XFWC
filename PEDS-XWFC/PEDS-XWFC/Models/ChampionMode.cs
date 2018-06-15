ded37f7cc8f1f5e4d260b9e01952a5a4d74e03bf
Randy <randyma01@gmail.com>
2018-06-14 20:34:53 -0600

    ChampionMode

---
+++ PEDS-XWFC/PEDS-XWFC/Models/ChampionMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PEDS_XWFC.Models
+{
+    public class ChampionMode
+    {
+        public int IdChampionMode { get; set; }
+        public Tournament Tournament { get; set; }
+        public NewUser FanUser { get; set; }
+
+
+    }
+}
+
