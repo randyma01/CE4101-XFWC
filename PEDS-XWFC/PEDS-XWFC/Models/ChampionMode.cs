f1527ca9541106fc6029a3c594308dee57b6ed5d
Randy <randyma01@gmail.com>
2018-06-18 11:21:30 -0600

    Modo Champion "da champions"

--- PEDS-XWFC/PEDS-XWFC/Models/ChampionMode.cs
+++ PEDS-XWFC/PEDS-XWFC/Models/ChampionMode.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PEDS_XWFC.Controllers;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +11,29 @@ namespace PEDS_XWFC.Models
     {
         public int IdChampionMode { get; set; }
         public Tournament Tournament { get; set; }
-        public NewUser FanUser { get; set; }
+        public int IdUserFanatic { get; set; }
+        public Prediction Prediction { get; set; }
 
 
+
+        public void LoadGames(string idTournament)
+        {
+            Connection connection = new Connection();
+            DataView dataView;
+
+            string query = "SELECT  partido.IdPartido, seleccion.NombreSeleccion AS Selecciones FROM" + 
+                "partido INNER JOIN seleccion_partido ON partido.IdPartido = seleccion_partido.IdPartido" +
+                "INNER JOIN seleccion ON seleccion_partido.IdSeleccion = seleccion.IdSeleccion WHERE" + idTournament;
+
+            dataView = connection.getData(query);
+
+            foreach (DataRowView datarow in dataView)
+            {
+                string selecciones = datarow["Selecciones"].ToString();
+                string idPartido = datarow["IdPartido"].ToString();
+            }
+
+        }
 
     }
 }
